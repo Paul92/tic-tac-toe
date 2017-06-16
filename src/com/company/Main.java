5f5da3094aea5fad34c33b772c89db1609ba8a65
Conner Snyder <cjsnyder1018@gmail.com>
2017-06-16 15:57:03 -0500

    added board

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    private static String[][] board = new String[3][3];
+    public static void main(String[] args) {
+        board();
+
+    }
+
+    public static void board(){
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = "";
+            }
+            System.out.println(Arrays.deepToString(board[i]));
+        }
+    }
+
+    public static void checker(){
+        
+    }
+
+}
