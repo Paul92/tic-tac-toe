c57dc2f8a0159bb66147d42743e30641d653f131
cjsnyder <cjsnyder1018@gmail.com>
2017-06-16 21:30:25 -0500

    cleaned up code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,40 @@
 package com.company;
 
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
 
     private static String[][] board = new String[3][3];
-    public static String playerOne = "X";
-    public static String playerTwo = "O";
-    public static Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
 
-    public static int turn;
-    public static int rowChoice;
-    public static int colChoice;
-
+    private static int turn;
 
     public static void main(String[] args) {
         board();
         runGame();
     }
 
-    public static void board(){
+    private static void board(){
         for (int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 board[i][j] = "";
             }
         }
     }
-    public static void runGame(){
+    private static void runGame(){
         while (turn < 9){
             System.out.print("Enter a row (1-3): ");
-            rowChoice = sc.nextInt() - 1;
+            int rowChoice = sc.nextInt() - 1;
             System.out.print("Enter a column (1-3): ");
-            colChoice = sc.nextInt() - 1;
-            if (board[rowChoice][colChoice] == "" && turn % 2 == 0){
-                board[rowChoice][colChoice] = "X";
-            } else if (board[rowChoice][colChoice] == "" && turn % 2 != 0){
-                board[rowChoice][colChoice] = "O";
+            int colChoice = sc.nextInt() - 1;
+            if (Objects.equals(board[rowChoice][colChoice], "") && turn % 2 == 0){
+                String playerOne = "X";
+                board[rowChoice][colChoice] = playerOne;
+            } else if (Objects.equals(board[rowChoice][colChoice], "") && turn % 2 != 0){
+                String playerTwo = "O";
+                board[rowChoice][colChoice] = playerTwo;
             } else {
                 System.out.println("That space is taken! Try again!");
                 turn--;
